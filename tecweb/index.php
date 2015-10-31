b27cecffb10653887841d719797729b0d3798772
desprograme <cris@desprograme.com.br>
2015-10-31 13:20:15 -0200

    Update index.php

--- tecweb/index.php
+++ tecweb/index.php
@@ -10,10 +10,10 @@ include "funcoes.php";
 
 
 echo "<div class='regiao3'>
-			<h2>Faculdade Impacta</h2><h3>Trabalho TecWeb</h3>
-			<p>No início, os computadores eram tidos apenas como 'máquinas gigantes' que tornavam possível a automatização de determinadas tarefas em instituições de ensino/pesquisa, grandes empresas e nos meios governamentais. </p>
-			<p>Com o avanço tecnológico, tais máquinas começaram a perder espaço para equipamentos cada vez menores, mais poderosos e mais confiáveis. </p>
-			<p>Como se não bastasse, a evolução das telecomunicações permitiu que, aos poucos, os computadores passassem a se comunicar, mesmo estando em lugares muito distantes geograficamente.</p>
+	<h2>Faculdade Impacta</h2><h3>Trabalho TecWeb</h3>
+	<p>No início, os computadores eram tidos apenas como 'máquinas gigantes' que tornavam possível a automatização de determinadas tarefas em instituições de ensino/pesquisa, grandes empresas e nos meios governamentais. </p>
+	<p>Com o avanço tecnológico, tais máquinas começaram a perder espaço para equipamentos cada vez menores, mais poderosos e mais confiáveis. </p>
+	<p>Como se não bastasse, a evolução das telecomunicações permitiu que, aos poucos, os computadores passassem a se comunicar, mesmo estando em lugares muito distantes geograficamente.</p>
 
 </div>
 </html>";
